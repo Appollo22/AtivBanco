df0be64953c832ee59a46e755e14847c07a4207d
Nathan Spanhol <nathanespanhol@hotmail.com>
2026-03-22 18:01:39 -0400

    2 e 3

--- ATIV/ScriptAtiv.sql
+++ ATIV/ScriptAtiv.sql
@@ -5,3 +5,22 @@
 select nome, 'Aluno' as classificacao from alunos
 union all
 select nome, 'Professor' as classificacao from professores
+
+/* 2. Reescreva o exercício 1 usando UNION (sem ALL). O resultado muda? Explique por quê no comentário SQL.
+*/
+
+select nome, 'Aluno' as classificacao from alunos
+union
+select nome, 'Professor' as classificacao from professores
+
+/* Não muda nada devido a todos os nomes serem diferentes */
+
+/* 3. Monte uma query que retorne os nomes das avaliações distintas que existem na tabela notas. Faça de duas
+formas: com DISTINCT e com UNION.
+*/
+
+select distinct avaliacao from notas
+
+select avaliacao, 'Avalicao' as tipo from notas
+union
+select avaliacao, 'Avalicao' as tipo from notas
