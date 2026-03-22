52dfe83e58b48419d2175c24c6bdbb8ef87864e1
Nathan Spanhol <nathanespanhol@hotmail.com>
2026-03-22 18:33:45 -0400

    4 e 5

--- ATIV/ScriptAtiv.sql
+++ ATIV/ScriptAtiv.sql
@@ -24,3 +24,21 @@ select distinct avaliacao from notas
 select avaliacao, 'Avalicao' as tipo from notas
 union
 select avaliacao, 'Avalicao' as tipo from notas
+
+/* 4. Crie uma VIEW chamada vw_boletim que mostre: nome do aluno, nome da disciplina, avaliação e nota. Use
+os JOINs necessários (notas → matriculas → alunos, matriculas → turmas → disciplinas).
+*/
+
+create view vw_boletim as
+select a.nome as aluno, d.nome as disciplina, n.avaliacao, n.nota, t.semestre
+from notas n
+join matriculas m on m.id = n.matricula_id
+join alunos a on a.id = m.aluno_id
+join turmas t on t.id = m.turma_id
+join disciplinas d on d.id = t.disciplina_id; 
+
+/* 5. Usando a vw_boletim, escreva uma query que retorne a média de cada aluno por disciplina.
+*/
+
+select aluno, disciplina, avg(nota) as media from vw_boletim
+group by aluno, disciplina
